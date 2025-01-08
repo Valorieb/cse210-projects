7ef4f867e7658c3211615db7c91b7dfac7ead3a6
Valorieb <valoriebroderick@gmail.com>
2025-01-08 15:01:47 -0600

    Completed exercise2.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("\nWhat is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letter;
+        bool pass = false;
+
+        if(percent >= 70){
+            pass = true;
+        }
+    
+        if(percent >= 90){
+            letter = "A";
+        } else if (percent >= 80){
+            letter = "B";
+        } else if (percent >=70){
+            letter ="C";
+        }else if (percent >=60){
+            letter ="D";
+        }else {
+            letter ="F";
+        }
+
+        // I wanted to practice using functions, so I did it a little different.
+        PrintGrade(letter, pass);
+
     }
+    static void PrintGrade(string letter, bool pass){
+       Console.WriteLine($"Your grade is {letter}.");
+       if(pass){
+        Console.WriteLine($"Congratulations! You passed!\n");
+       }else{
+        Console.WriteLine($"You didn't pass this time. Keep trying!\n");
+       }
+        }
 }
