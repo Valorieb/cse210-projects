49c0ff1971a3b2b749801898158b1c1f77272707
Valorieb <valoriebroderick@gmail.com>
2025-01-10 10:08:25 -0600

    Completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNum;
+        int guess = -1;
+
+        Random random = new Random();
+        magicNum = random.Next(101);
+
+        while(guess != magicNum){
+            Console.WriteLine("What is your guess? ");
+            string userInput1 = Console.ReadLine();
+            guess = int.Parse(userInput1);
+
+            if(guess > magicNum){
+                Console.WriteLine("Lower");
+            }else if(guess < magicNum){
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
