0ed5f314488527989ac15a03623de6ca1633aa51
Valorieb <valoriebroderick@gmail.com>
2025-01-21 12:44:28 -0600

    Completed W03 Encapsulation Activity.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction defaultFraction = new Fraction();
+        Console.WriteLine($"Frac1: {defaultFraction.GetTop()}/{defaultFraction.GetBottom()}");
+
+        Fraction oneFraction = new Fraction(5);
+        Console.WriteLine($"Frac2: {oneFraction.GetTop()}/{oneFraction.GetBottom()}");
+
+        Fraction twoFraction = new Fraction(2, 7);
+        Console.WriteLine($"Frac3: {twoFraction.GetTop()}/{twoFraction.GetBottom()}");
+
+        double decDefault = defaultFraction.GetDecimalValue();
+        Console.WriteLine(decDefault);
+        string strDefault = defaultFraction.GetFractionString();
+        Console.WriteLine(strDefault);
+
+        double decOne = oneFraction.GetDecimalValue();
+        Console.WriteLine(decOne);
+        string strOne = oneFraction.GetFractionString();
+        Console.WriteLine(strOne);
+
+        double decTwo = twoFraction.GetDecimalValue();
+        Console.WriteLine(decTwo);
+        string strTwo = twoFraction.GetFractionString();
+        Console.WriteLine(strTwo);
+
+
     }
 }
