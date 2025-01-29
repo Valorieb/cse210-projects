c763c4989a0e3f7f012de9391d44bcc05264eb07
Valorieb <valoriebroderick@gmail.com>
2025-01-29 15:16:06 -0600

    Completed W04 Assignments

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,10 +1,11 @@
-public class Reference{
-// Keeps track of the book, chapter, and verse information.
+public class Reference
+{
+    // Keeps track of the book, chapter, and verse information.
     private string _book;
     private int _chapter;
     private int _verse;
     private int _endVerse;
-    
+
     public Reference(string book, int chapter, int verse)
     {
         _book = book;
@@ -12,23 +13,30 @@ public class Reference{
         _verse = verse;
         _endVerse = 0;
     }
-    public Reference(string book, int chapter, int startVerse, int endVerse){
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
         _book = book;
         _chapter = chapter;
         _verse = startVerse;
         _endVerse = endVerse;
     }
-    public string GetDisplayTextSingle(){
-            return($"{_book} {_chapter}:{_verse}");
-        }
-    public string GetDisplayTextMultiple(){
-        return($"{_book} {_chapter}:{_verse}-{_endVerse}");
+    public string GetDisplayTextSingle()
+    {
+        return ($"{_book} {_chapter}:{_verse}");
     }
-    public string GetDisplayText(){
-        if(_endVerse == 0){
-            return($"\n{_book} {_chapter}:{_verse}");
-        }else{
-            return($"\n{_book} {_chapter}:{_verse}-{_endVerse}");
+    public string GetDisplayTextMultiple()
+    {
+        return ($"{_book} {_chapter}:{_verse}-{_endVerse}");
+    }
+    public string GetDisplayText()
+    {
+        if (_endVerse == 0)
+        {
+            return ($"\n{_book} {_chapter}:{_verse}");
+        }
+        else
+        {
+            return ($"\n{_book} {_chapter}:{_verse}-{_endVerse}");
         }
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        VideoLibrary videoLibrary = new VideoLibrary();
+        string viewLibrary = videoLibrary.DisplayLibrary();
+        Console.WriteLine(viewLibrary);
+
     }
 }
