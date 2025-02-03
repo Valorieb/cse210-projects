c183c677ffd051879cc54e3cc00ce7ea665aa15b
Valorieb <valoriebroderick@gmail.com>
2025-02-03 16:50:21 -0600

    Completed W05 HW assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,23 @@
 using System;
-
+using Homework;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Fred", "Biology");
+        string summary = assignment1.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment math1 = new MathAssignment("Gabe", "Algebra","Section 5.2", "Problems 1-20");
+        string hwList1 = math1.GetHomeworkList();
+        string mathSummary1 = math1.GetSummary();
+        Console.Write(mathSummary1);
+        Console.WriteLine(hwList1);
+
+        WritingAssignment write1 = new WritingAssignment("Anna", "Shakespeare", "Hamlet");
+        Console.Write(write1.GetSummary());
+        Console.WriteLine(write1.GetWritingInformation());
     }
+
+    
 }
